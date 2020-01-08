24efe707521cba4741eee965078e99ad51d06ca8
Andrew Westwick <awestwick@gmail.com>
2020-01-07 22:02:00 -0600

    add remaining functionality for add and remove

--- src/App.vue
+++ src/App.vue
@@ -2,22 +2,27 @@
   <div id="app">
     <h2>My List</h2>
     <div class="my-list">
+      <p v-if="myList.length <= 0">No titles to display</p>
       <Title v-for="title in myList"
         :key="title.id"
-        :title="title.title"
-        :image="title.img"
+        :title="title"
+        :buttonText="'Remove'"
+        @titleClicked="removeTitle(title)"
       >
       </Title>
     </div>
     <h2>Recommendations</h2>
     <div class="recommendations">
-      <Title v-for="title in myList"
+      <p v-if="recommendations.length <= 0">No recommendations, check back soon!</p>
+      <Title v-for="title in recommendations"
         :key="title.id"
-        :title="title.title"
-        :image="title.img"
+        :title="title"
+        :buttonText="'Add'"
+        @titleClicked="addTitle(title)"
       >
       </Title>
     </div>
+    <p>Selected Titles ({{ myList.length }}): {{ selectedTitles }}</p>
   </div>
 </template>
 
@@ -35,6 +40,24 @@ export default {
   },
   components: {
     Title,
+  },
+  computed: {
+    selectedTitles() {
+      return this.myList.length ? this.myList.map(title => title.title).join(', ') : 'No titles selected';
+    },
+  },
+  methods: {
+    addTitle(titleToAdd) {
+      if (!this.myList.includes(titleToAdd)) {
+        this.myList.push(titleToAdd);
+      } else {
+        // what to do if title already in my list?
+        // pop up a toast maybe?
+      }
+    },
+    removeTitle(titleToRemove) {
+      this.myList = this.myList.filter(title => title.id !== titleToRemove.id);
+    },
   },
 };
 </script>
--- src/components/Title.vue
+++ src/components/Title.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="title">
-    <img :src="image" />
-    <p>{{ title }}</p>
+    <div class="img-wrap">
+      <img :src="title.img" />
+      <button @click="handleClick(title)">{{ buttonText }}</button>
+    </div>
+    <p>{{ title.title }}</p>
   </div>
 </template>
 
@@ -9,8 +12,14 @@
 export default {
   name: 'Title',
   props: {
-    title: String,
-    image: String,
+    title: Object,
+    buttonText: String,
+  },
+  methods: {
+    handleClick(title) {
+      console.log('title clicked', title);
+      this.$emit('titleClicked', title);
+    },
   },
 };
 </script>
@@ -21,5 +30,37 @@ export default {
   display: flex;
   flex-direction: column;
   margin-right: 32px;
+
+  img {
+    height: 220px;
+    width: auto;
+  }
+
+  button {
+    background: rgba(0,0,0,.67);
+    display: none;
+    padding: 0.35em 1.2em;
+    border: 2px solid #111;
+    box-sizing: border-box;
+    text-decoration: none;
+    color: #fff;
+    text-align: center;
+    position: absolute;
+    text-transform: uppercase;
+    font-size: 18px;
+    font-weight: bold;
+    width: 100%;
+    bottom: 0;
+    left: 0;
+    cursor: pointer;
+  }
+
+  .img-wrap {
+    position: relative;
+    height: 220px;
+    &:hover button {
+      display: inline-block;
+    }
+  }
 }
 </style>
