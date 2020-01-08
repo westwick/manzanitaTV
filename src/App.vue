0a0e42653a790f94144b4c9ca54ddfedce327c60
Andrew Westwick <awestwick@gmail.com>
2020-01-07 21:28:26 -0600

    import data into app and display titles

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,35 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h2>My List</h2>
+    <div class="my-list">
+      <div class="title" v-for="title in myList" :key="title.id">
+        <img :src="title.img" />
+        <p>{{title.title}}</p>
+      </div>
+    </div>
+    <h2>Recommendations</h2>
+    <div class="recommendations">
+      <div class="title" v-for="title in recommendations" :key="title.id">
+        <img :src="title.img" />
+        <p>{{title.title}}</p>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue';
+import mockdata from './data/mockdata.json';
 
 export default {
   name: 'app',
+  data() {
+    return {
+      myList: mockdata.mylist,
+      recommendations: mockdata.recommendations,
+    };
+  },
   components: {
     HelloWorld,
   },
@@ -17,12 +37,23 @@ export default {
 </script>
 
 <style lang="scss">
+body {
+  background: #f8f8f8;
+  padding: 0 16px;
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+.my-list, .recommendations {
+  display: flex;
+}
+
+.title {
+  display: flex;
+  flex-direction: column;
+  margin-right: 32px;
 }
 </style>
